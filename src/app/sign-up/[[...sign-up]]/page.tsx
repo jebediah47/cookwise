1d18b1e9b5f36c21aac169750b4eb5b789c67a5b
Christian Llupo <jebediah47@protonmail.com>
2025-10-24 23:25:25 +0300

    Set `forceRedirectUrl` prop for `SignIn` and `SignUp` components

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center items-center h-screen bg-muted/40">
-      <SignUp />
+      <SignUp forceRedirectUrl="/app" />
     </div>
   );
 }
