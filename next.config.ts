926c17df907187c2e5543054bc3cfde2a2aa7236
Christian Llupo <jebediah47@protonmail.com>
2025-10-26 11:13:48 +0200

    CSP was Traefik's fault so fuck it, bye bye CSP

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,5 @@
 import type { NextConfig } from 'next';
 
-// Define the Content Security Policy
-// This policy allows all the domains you are getting errors for.
-const cspHeader = `
-    default-src 'self' * 'unsafe-inline' 'unsafe-eval';
-    script-src 'self' * 'unsafe-inline' 'unsafe-eval';
-    style-src 'self' * 'unsafe-inline';
-    img-src 'self' * blob: data:;
-    font-src 'self' * data:;
-    worker-src 'self' * blob:;
-    frame-src 'self' *;
-    connect-src 'self' *;
-`;
-
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -38,19 +25,6 @@ const nextConfig: NextConfig = {
     ],
   },
   output: 'standalone',
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
